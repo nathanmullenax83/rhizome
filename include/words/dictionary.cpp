358b8a0603a4c1aeaf096fa50ee2e8092abe1f79
Nathan Mullenax <nathanmullenax83@github.com>
2020-06-27 00:04:34 +0000

    Added rhizome::words namespace.

---
+++ include/words/dictionary.cpp
@@ -0,0 +1,119 @@
+#include "dictionary.hpp"
+
+#include <algorithm>
+#include <vector>
+
+using std::sort;
+using std::vector;
+
+namespace rhizome {
+    namespace words {
+        Dictionary::Dictionary() {
+
+        }
+
+        Dictionary::~Dictionary() {
+
+        }
+
+        size_t 
+        Dictionary::bits() const {
+            size_t size = counts.size();
+            // find the nearest power of two
+            size_t n = 1;
+            while( ((size_t)(2 << n)) < size ) {
+                ++n;
+            }
+            return n;
+        }
+
+        Integer
+        Dictionary::id( string const &n ) {
+            if( counts.count(n) > 0 ) {
+                counts[n].first += 1;
+                counts[n].second += n.size();
+            } else {
+                counts[n].first = 1;
+                counts[n].second = n.size();
+            }
+            return counts[n].second;
+        }
+
+        void
+        Dictionary::update_table() {
+            string w;
+            
+            size_t length(0);
+            size_t count(0);
+
+            
+
+            // auto opt_order = [&m](string w){
+            //     return &m[w].first;
+            // };
+
+            // auto nat_order = [&m](string w) {
+            //     return &m[w].second;
+            // };
+            vector< pair<string,size_t> > n;
+            // Def: ideal order is count*ordinal should be kept minimal.
+            //      the most concise table assigns the shortest prefix
+            //      to the longest, most frequent word.
+            for( auto i=counts.begin(); i!=counts.end(); i++) {
+                w = i->first;
+                length = i->first.size();
+                count = i->second.first;
+                n.push_back( pair<string,size_t>(w,count*length));
+            }
+
+            std::sort( n.begin(), n.end(),  
+            []( const auto & a, const auto & b) {
+                    return a.second > b.second;
+                } );
+
+            for( auto i=n.begin(); i!=n.end(); i++) {
+                counts[i->first].second = i->second;
+            }
+        }
+
+        string
+        Dictionary::rhizome_type() const {
+            return "words::Dictionary";
+        }
+
+        void
+        Dictionary::serialize_to( std::ostream &out ) const {
+            out << rhizome_type();
+            out << "{\n";
+            for( auto i=counts.begin(); i!=counts.end(); i++) {
+                out << "\t";
+                out << i->first << ":" << i->second.first << "," << i->second.second;
+                if( i!=counts.end() ) {
+                    out << ",\n";
+                } else {
+                    out << "\n";
+                }
+            }
+            out << "}";
+        }
+
+        bool
+        Dictionary::has_interface( string const &name ) {
+            return name=="Thing" || name==rhizome_type();
+        }
+
+        Thing *
+        Dictionary::invoke(string const &method, Thing *arg) {
+            (void)method;
+            (void)arg;
+            throw runtime_error("Not implemented.");
+        }
+
+        Thing *
+        Dictionary::clone() const {
+            Dictionary *copy = new Dictionary();
+            copy->counts = counts;
+            return copy;
+        }
+    }
+}
---
+++ include/words/dictionary.hpp
@@ -0,0 +1,50 @@
+#ifndef RHIZOME_WORDS_DICTIONARY_HPP
+#define RHIZOME_WORDS_DICTIONARY_HPP
+
+#include <map>
+#include <string>
+#include <tuple>
+#include "types/integer.hpp"
+
+using rhizome::types::Integer;
+using std::map;
+using std::string;
+using std::pair;
+
+#include "core/thing.hpp"
+
+using rhizome::core::Thing;
+
+namespace rhizome {
+    namespace words {
+        /// encoding table for words
+        class Dictionary: public Thing {
+        private:
+            /// string: count*position
+            map< string, pair<size_t,size_t> > counts;
+            
+            /// Update the table 
+            void update_table();
+            
+            /// compute number of bits needed to specify a 
+            /// member of this dictionary
+            
+        public:
+            Dictionary();
+            ~Dictionary();
+
+            Integer id( string const &s );
+
+            size_t bits() const;
+
+            virtual void serialize_to( std::ostream &out ) const override;
+            virtual string rhizome_type() const override;
+            virtual bool has_interface( string const &name ) override;
+            virtual Thing * clone() const override;
+            virtual Thing * invoke ( string const &method, Thing *arg ) override;
+
+        };
+    }
+}
+
+#endif
